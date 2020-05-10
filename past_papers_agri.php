e466d69e83b2d1d3508bdb8927f5f42ab24cfffe
Rukshan J. Senanayaka <43839071+RukshanJS@users.noreply.github.com>
2020-05-10 19:46:46 +0530

    Update past_papers_agri.php

--- past_papers_agri.php
+++ past_papers_agri.php
@@ -35,7 +35,7 @@ $ftp_user = "manusath";
 $ftp_password = "MH2020@rt";
 
 // set up a connection or die
-$conn_id = ftp_connect($ftp_host) or die("Couldn't connect to $ftp_host"); 
+$conn_id = ftp_connect($ftp_host,21) or die("Couldn't connect to $ftp_host"); 
 
 // try to login
 if (@ftp_login($conn_id, $ftp_user, $ftp_password)) {
